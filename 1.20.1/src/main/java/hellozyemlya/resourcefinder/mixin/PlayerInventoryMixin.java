c4fbcbefe22c8e93941d644b44041530f155be95
Yevhenii Chekanskiy <echekanskiy@gmail.com>
2023-07-30 15:09:14 +0300

    Some mixin experiments.

--- 1.20.1/src/main/java/hellozyemlya/resourcefinder/mixin/PlayerInventoryMixin.java
+++ 1.20.1/src/main/java/hellozyemlya/resourcefinder/mixin/PlayerInventoryMixin.java
@@ -1,16 +1,21 @@
 package hellozyemlya.resourcefinder.mixin;
 
+import hellozyemlya.resourcefinder.DefaultedListWithSetCallback;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 
 @Mixin(PlayerInventory.class)
 public class PlayerInventoryMixin {
-    @Inject(method = "insertStack(ILnet/minecraft/item/ItemStack;)Z", at=@At(value = "RETURN"))
-    private void logInsert(int slot, ItemStack stack, CallbackInfoReturnable<Boolean> cir){
-        System.out.printf("Stack %s inserted to inventory\n", stack.toString());
+    @Redirect(method = "<init>", at=@At(value = "INVOKE", target = "Lnet/minecraft/util/collection/DefaultedList;ofSize(ILjava/lang/Object;)Lnet/minecraft/util/collection/DefaultedList;"))
+    private <E> DefaultedList<E>  redirectCreation(int size, E defaultValue){
+        return (DefaultedList<E>)DefaultedListWithSetCallback.createOfSize(size, (ItemStack)defaultValue);
     }
 }
